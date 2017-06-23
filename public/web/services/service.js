09ad9a0843c14f2632ed8b041a1e78e2619d069e
Kohpai <whcwhc78@gmail.com>
2017-06-23 21:38:53 +0700

    accountService.login added to service

--- public/web/services/service.js
+++ public/web/services/service.js
@@ -1,1 +1,18 @@
 var myApp = angular.module("app.service", []);
+
+myApp.service('accountService', accountService);
+
+function accountService($firebaseAuth) {
+    return {
+        login: function() {
+            firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider())
+                .then(function (res) {
+                    // console.log('signInRes',res)
+                    console.log('login success', res)
+                })
+                .catch(function (error) {
+                    console.log(error)
+                });
+        },
+    };
+}
