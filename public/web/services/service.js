3736cde7befbfb3ee34b49b29ef0cbe3c117bbe1
Kohpai <whcwhc78@gmail.com>
2017-06-24 02:23:53 +0700

    changed accountService to auth object

--- public/web/services/service.js
+++ public/web/services/service.js
@@ -4,18 +4,7 @@ myApp.service('accountService', accountService);
 myApp.service('urlService', urlService);
 
 function accountService($firebaseAuth) {
-    return {
-        login: function() {
-            firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider())
-                .then(function (res) {
-                    // console.log('signInRes',res)
-                    console.log('login success', res)
-                })
-                .catch(function (error) {
-                    console.log(error)
-                });
-        },
-    };
+    return auth;
 }
 
 function urlService($location) {
