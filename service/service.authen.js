d8814b1077a982d7c46d17c866a828dc5e1b6d4b
Krittanon.w <krittanon.w@gmail.com>
2017-06-23 23:39:09 +0700

    move to promise and refect

--- service/service.authen.js
+++ service/service.authen.js
@@ -1,10 +1,51 @@
-function login() {
-    firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider())
-        .then(function (res) {
-            // console.log('signInRes',res)
-            console.log('login success', res)
-        })
-        .catch(function (error) {
-            console.log(error)
-        })
+const auth = {
+    login: function () {
+        console.log('login: called')
+        return firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider())
+            .then((result) => {
+                console.log('login: success', result)
+            })
+            .catch(function (error) {
+                console.log('login: failed', error)
+            })
+    },
+    logout: function () {
+        console.log('logout: called')
+        return firebase.auth().signOut()
+            .then(() => {
+                console.log('logout: success')
+            })
+            .catch(function (error) {
+                console.log('logout: failed', error)
+            })
+    },
+    getUser() {
+        console.log('getUser: called')
+        return new Promise((resolve, reject) => {
+            var user = firebase.auth().currentUser
+            if (user != null) {
+                console.log('getUser: success', user)
+                resolve(user);
+            } else {
+                console.log('getUser: failed')
+                reject(Error("unknow user"));
+            }
+        });
+    }
 }
+
+
+
+// function login() {
+//     firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider())
+//         .then(function (res) {
+//             console.log('login success', res)
+//         })
+//         .catch(function (error) {
+//             console.log(error)
+//         })
+// }
+
+// function getCuttentUser() {
+//     var user = firebase.auth().currentUser
+// }
