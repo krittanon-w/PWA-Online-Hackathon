c53c5fd57930d26662f989fecb580ee634d44c06
Kohpai <whcwhc78@gmail.com>
2017-06-24 03:32:53 +0700

    userService.setInfo added to service

--- public/web/services/service.js
+++ public/web/services/service.js
@@ -2,6 +2,7 @@ var myApp = angular.module("app.service", []);
 
 myApp.service('accountService', accountService);
 myApp.service('urlService', urlService);
+myApp.service('userService', userService);
 
 function accountService($firebaseAuth) {
     return auth;
@@ -11,6 +12,23 @@ function urlService($location) {
     return {
         server: function() {
             return $location.protocol() + '://'+ $location.host() + ':' + $location.port() + '/#!';
+        },
+    };
+}
+
+function userService() {
+    return {
+        setInfo(inputInfo) {
+            var userInfo = auth.getUserInfo();
+            firebase.database.ref('/users/' + userInfo.id).set({
+                displayName: userInfo.displayName,
+                photoURL: userInfo.photoURL,
+                email: userInfo.email,
+                firstName: inputInfo.firstName,
+                lastName: inputInfo.lastName,
+                age: inputInfo.age,
+                gender: inputInfo.gender
+            });
         },
     };
 }
