b116af70d92a361077c75337790aca1a7544dc62
toliewfate <sonchai.poomipak@gmail.com>
2017-06-24 14:00:03 +0700

    edit select page

--- public/web/controllers/accostController.js
+++ public/web/controllers/accostController.js
@@ -1,13 +1,25 @@
 var myApp = angular.module("app.accost", []);
 
 
-myApp.controller('SelectController', function($scope, $location, urlService) {
+myApp.controller('SelectController', function($scope, $location, urlService, matchingService) {
     // Parameter Start
+    $scope.accounts = "";
     // Parameter End
 
     // Function Start
     $scope.init = function() {
+        matchingService.getUsers().then(function(resolve){
+            console.log(resolve);
+            $scope.accounts = resolve;
+            $scope.$apply();
+        }).catch(function(reject){
+
+        });
     };
+
+    $scope.choose = function(uid){
+        console.log(uid);
+    }
     // Function End
 
     // Directive Start
@@ -23,6 +35,39 @@ myApp.controller('SelectController', function($scope, $location, urlService) {
 
 myApp.controller('ListController', function($scope, $location, urlService) {
     // Parameter Start
+     $scope.accounts = [{
+        displayName: 'son poo',
+        firstName: 'son',
+        lastName: 'poo',
+        Age: '25',
+        detail:'sdafweatgawg',
+        photoURL:'https://lh6.googleusercontent.com/-9QHVXyYdoWM/AAAAAAAAAAI/AAAAAAAAABE/PzTK6xALEcU/photo.jpg',
+        uid:'65464131',
+    },{
+        displayName: 'son poo',
+        firstName: 'son',
+        lastName: 'poo',
+        Age: '25',
+        detail:'sdafweatgawg',
+        photoURL:'https://lh6.googleusercontent.com/-9QHVXyYdoWM/AAAAAAAAAAI/AAAAAAAAABE/PzTK6xALEcU/photo.jpg',
+        uid:'65464131',
+    },{
+        displayName: 'son poo',
+        firstName: 'son',
+        lastName: 'poo',
+        Age: '25',
+        detail:'sdafweatgawg',
+        photoURL:'https://lh6.googleusercontent.com/-9QHVXyYdoWM/AAAAAAAAAAI/AAAAAAAAABE/PzTK6xALEcU/photo.jpg',
+        uid:'65464131',
+    },{
+        displayName: 'son poo',
+        firstName: 'son',
+        lastName: 'poo',
+        Age: '25',
+        detail:'sdafweatgawg',
+        photoURL:'https://lh6.googleusercontent.com/-9QHVXyYdoWM/AAAAAAAAAAI/AAAAAAAAABE/PzTK6xALEcU/photo.jpg',
+        uid:'65464131',
+    }];
     // Parameter End
 
     // Function Start
