99007acf933149d54a0f5d50244d5b10f332f7b5
toliewfate <sonchai.poomipak@gmail.com>
2017-06-24 20:05:57 +0700

    include prediction link

--- public/web/controllers/accostController.js
+++ public/web/controllers/accostController.js
@@ -1,7 +1,7 @@
 var myApp = angular.module("app.accost", []);
 
 
-myApp.controller('SelectController', function($scope, $location, urlService, matchingService, userService, accountService) {
+myApp.controller('SelectController', function($scope, $location, urlService, matchingService, userService, accountService, dtreeService) {
     // Parameter Start
     $scope.accounts = "";
     $scope.show = {
@@ -15,7 +15,6 @@ myApp.controller('SelectController', function($scope, $location, urlService, mat
     // Function Start
     $scope.init = function() {
         accountService.getUserInfo().then(function(resolve){
-            console.log("resolve", resolve);
             if(resolve != null){
                 userService.getInfo(resolve.uid).then(function(resolve){
                     if(resolve.type != undefined){
@@ -40,7 +39,29 @@ myApp.controller('SelectController', function($scope, $location, urlService, mat
     };
 
     $scope.submit = function(uid){
-        console.log($scope.questions);
+        accountService.getUserInfo().then(function(resolve){
+            if(resolve != null){
+                userService.getInfo(resolve.uid).then(function(resolve){
+                    if(resolve != null){
+                        var traningData = {
+                            age: resolve.age,
+                            gender: resolve.gender,
+                            q1: $scope.questions[0],
+                            q2: $scope.questions[1],
+                            q3: $scope.questions[2],
+                            q4: $scope.questions[3],
+                            q5: $scope.questions[4],
+                        };
+                        dtreeService.addTraningData(traningData).then(function(resolve){
+                            console.log(resolve);
+                        }).catch(function(reject){
+                        })
+                    }
+                }).catch(function(reject){
+                });
+            }
+        }).catch(function(reject){
+        })
     };
 
     $scope.choose = function(uid){
